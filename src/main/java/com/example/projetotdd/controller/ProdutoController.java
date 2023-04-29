e99d281b1c3e14af6723b609d3e0c7f5b4d3e749
Elton Standerski <eltonstan@yahoo.com.br>
2023-04-29 11:47:53 -0300

    Adicionado novos testes controller

--- src/main/java/com/example/projetotdd/controller/ProdutoController.java
+++ src/main/java/com/example/projetotdd/controller/ProdutoController.java
@@ -1,9 +1,9 @@
 package com.example.projetotdd.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,25 +14,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.projetotdd.model.Produto;
+import com.example.projetotdd.service.ProdutoService;
 
 @RestController
 @RequestMapping(path = "/api/produtos")
 public class ProdutoController {
 	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	@GetMapping
 	public ResponseEntity<List<Produto>> obterTodos() {
-		List<Produto> produtos =  new ArrayList<Produto>();
+		List<Produto> produtos = produtoService.obterTodos();
 		return new ResponseEntity<>(produtos, HttpStatus.OK);
 	}
 	
 	@GetMapping(path="{id}")
 	public ResponseEntity<Optional<Produto>> obterPorId(@PathVariable Long id) {
-		Optional<Produto> produto = Optional.of(new Produto());
+		Optional<Produto> produto = produtoService.obterPorId(id);
 		return new ResponseEntity<>(produto, HttpStatus.OK);
 	}
 	
 	@PostMapping
-	public ResponseEntity<Produto> obterPorId(@RequestBody Produto produto) {
+	public ResponseEntity<Produto> adicionar(@RequestBody Produto produtoRequest) {
+		Produto produto = produtoService.adicionar(produtoRequest);
 		return new ResponseEntity<>(produto, HttpStatus.CREATED);
 	}
 
--- src/test/java/com/example/projetotdd/controller/ProdutoControllerTest.java
+++ src/test/java/com/example/projetotdd/controller/ProdutoControllerTest.java
@@ -1,13 +1,25 @@
 package com.example.projetotdd.controller;
 
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.example.projetotdd.model.Produto;
+import com.example.projetotdd.service.ProdutoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
 public class ProdutoControllerTest {
@@ -15,8 +27,12 @@ public class ProdutoControllerTest {
 	@Autowired
 	private ProdutoController produtoController;
 	
+	@MockBean
+	private ProdutoService produtoService;
+
 	@Autowired
 	private MockMvc mockMvc;
+	
 
 	@BeforeEach
 	public void setup() {
@@ -26,10 +42,47 @@ public class ProdutoControllerTest {
 	@Test
 	public void deve_retornar_status_200_ao_chamar_o_metodo_obter_todos() throws Exception {
 		
-		// List<Produto> produtos = new ArrayList<Produto>();
-		var requestBuilder = MockMvcRequestBuilders.get("/api/produtos"); 
+		List<Produto> produtos = new ArrayList<Produto>();
+		var requestBuilder = MockMvcRequestBuilders.get("/api/produtos");
+		
+		when(this.produtoService.obterTodos()).thenReturn(produtos);
 		
 		this.mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isOk());
+		
+	}
+	
+	@Test
+	public void deve_retornar_o_produto_por_id() throws Exception {
+		
+		Produto produto = new Produto();
+		produto.setId(2L);
+		produto.setNome("Martelo");
+		produto.setQuantidade(10);
+		
+		Optional<Produto> optProduto = Optional.of(produto);
+		var requestBuilder = MockMvcRequestBuilders.get("/api/produtos/2");
+		
+		when(this.produtoService.obterPorId(2L)).thenReturn(optProduto);
+		
+		this.mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2L));
+		
+	}
+	
+	@Test
+	public void deve_adicionar_o_produto() throws Exception {
+		
+		Produto produto = new Produto();
+		produto.setNome("Martelo");
+		produto.setQuantidade(10);
+		
+		String json = new ObjectMapper().writeValueAsString(produto);
+		var requestBuilder = MockMvcRequestBuilders.post("/api/produtos/")
+				.content(json).contentType(MediaType.APPLICATION_JSON);
+		
+		produto.setId(1L);
+		when(this.produtoService.adicionar(produto)).thenReturn(produto);
+		
+		this.mockMvc.perform(requestBuilder).andExpect(MockMvcResultMatchers.status().isCreated());
 		
 	}
 
